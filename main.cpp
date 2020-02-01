a9c10eb1c0b315db49f636a88cf1120b8443488d
Fry98 <filip-toman@seznam.cz>
2020-02-02 00:22:26 +0100

    Added CLI

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <future>
 #include "matrix.hpp"
 
 void parse_line(Matrix& mtx, const std::string& line, int& line_length) {
@@ -28,17 +29,21 @@ void parse_line(Matrix& mtx, const std::string& line, int& line_length) {
   }
 }
 
-void fillFromStd(Matrix& mtx) {
+double matrix_from_std() {
+  Matrix mtx;
   std::string line;
   int line_length = -1;
 
   while (std::getline(std::cin, line)) parse_line(mtx, line, line_length);
   mtx.setWidth(line_length);
+  if (!mtx.isSquare()) throw std::exception("Invalid matrix size");
+  return mtx.getDeterminant();
 }
 
-void fillFromFile(Matrix& mtx, const char* name) {
+double matrix_from_file(const std::string name) {
   std::ifstream file;
   std::string line;
+  Matrix mtx;
   int line_length = -1;
 
   file.open(name);
@@ -46,21 +51,92 @@ void fillFromFile(Matrix& mtx, const char* name) {
   while (std::getline(file, line)) parse_line(mtx, line, line_length);
   mtx.setWidth(line_length);
   file.close();
+  if (!mtx.isSquare()) throw std::exception("Invalid matrix size");
+  return mtx.getDeterminant();
 }
 
-int main(int argc, char* argv[]) {
-  try {
-    Matrix mtx;
-    // if (argc < 2) {
-    //   fillFromStd(mtx);
-    // } else {
-    //   fillFromFile(mtx, argv[1]);
-    // }
-    fillFromFile(mtx, "input.txt");
+void print_help() {
+  std::cout <<
+    "(--input | -i) <filename1>;<filename2>;...  <- List of input files containing matrices (If none are specified, the program will be expecting input from std)\n"
+    "(--output | -o) <filename>  <- Filename of the file where you want the results to be saved (If not provided, the results will be written into std)\n"
+    "--result-only | -ro  <- Outputs only the results without prefixing the index"
+    "--single-thread | -st  <- Forces program to run on a single thread\n"
+    "--perf-comp | -pc  <- Runs the computations in both multi-threaded and single-threaded mode and compares the preformance\n"
+    "--help  <- Prints the list of valid commands\n";
+}
 
-    if (!mtx.isSquare()) throw std::exception("Invalid matrix size");
-    std::cout << "Matrix:\n" << mtx;
-    std::cout << "\nDeterminant:\n" << mtx.getDeterminant() << "\n";
+int main(unsigned int argc, char* argv[]) {
+  bool res_only = false;
+  unsigned char thread_mode = 0;
+  char* inp = nullptr;
+  char* out = nullptr;
+
+  try {
+    for (unsigned int i = 1; i < argc; i++) {
+      if (strcmp(argv[i], "--result-only") == 0 || strcmp(argv[i], "-ro") == 0) {
+        res_only = true;
+      } else if (strcmp(argv[i], "--single-thread") == 0 || strcmp(argv[i], "-st") == 0) {
+        if (thread_mode == 0) thread_mode = 1;
+      } else if (strcmp(argv[i], "--perf-comp") == 0 || strcmp(argv[i], "-pc") == 0) {
+        thread_mode = 2;
+      } else if (strcmp(argv[i], "--input") == 0 || strcmp(argv[i], "-i") == 0) {
+        if (i + 1 >= argc) throw std::exception("Input flag requires path argument");
+        inp = argv[++i];
+      } else if (strcmp(argv[i], "--output") == 0 || strcmp(argv[i], "-o") == 0) {
+        if (i + 1 >= argc) throw std::exception("Output flag requires path argument");
+        out = argv[++i];
+      } else if (strcmp(argv[i], "--help") == 0) {
+        print_help();
+        return 0;
+      } else {
+        throw std::runtime_error("Invalid argument: \"" + std::string(argv[i]) + "\"");
+      }
+    }
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << "\nHere is the list of valid commands:\n";
+    print_help();
+    return 0;
+  }
+
+  try {
+    std::ostream* outs = &std::cout;
+    std::ostream* errs = &std::cerr;
+    std::ofstream outfile;
+    if (out != nullptr) {
+      outfile.open(out, std::ios_base::trunc);
+      if (!outfile.is_open()) throw std::exception("Invalid filename");
+      outs = &outfile;
+      errs = &outfile;
+    }
+    if (inp == nullptr) {
+      double det = matrix_from_std();
+      if (!res_only) *outs << "(1) ";
+      *outs << det;
+    } else {
+      std::string filename = "";
+      std::vector<std::future<double>> futures;
+      for (unsigned int i = 0; inp[i] != 0; i++) {
+        if (inp[i] != 59) {
+          filename += inp[i];
+        } else {
+          if (filename.size() == 0) throw std::exception("Invalid filename");
+          futures.push_back(std::async(&matrix_from_file, filename));
+          filename = "";
+        }
+      }
+      if (filename.size() != 0) {
+        futures.push_back(std::async(&matrix_from_file, filename));
+      }
+      for (unsigned int i = 0; i < futures.size(); i++) {
+        if (!res_only) *outs << "(" << i + 1 << ") ";
+        try {
+          *outs << futures[i].get() << "\n";
+        } catch (const std::exception& e) {
+          *errs << e.what() << "\n";
+        }
+      }
+    }
+    if (out != nullptr) outfile.close();
   } catch(const std::exception& e) {
     std::cerr << e.what() << "\n";
   }
